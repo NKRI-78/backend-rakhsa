949a5885bfcd8610de4a1de1f352d0cad6a8077c
Reihan Agam <reihanagam7@gmail.com>
2024-11-14 14:40:22 +0700

    update

--- src/models/Sos.js
+++ src/models/Sos.js
@@ -4,7 +4,7 @@ module.exports = {
 
     list: () => {
         return new Promise((resolve, reject) => {
-            var query = `SELECT s.uid, s.location, s.media, st.name AS type, s.lat, s.lng, s.country, s.time, s.user_id, s.user_agent_id
+            var query = `SELECT s.uid, s.location, s.media, st.name AS type, s.lat, s.lng, s.country, s.is_confirm, s.time, s.user_id, s.user_agent_id
             FROM sos s
             INNER JOIN sos_types st ON st.id = s.sos_type
             `
