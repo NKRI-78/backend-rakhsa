434322637bfb0d7a83f13a3f860f51cf87121399
Reihan Agam <reihanagam7@gmail.com>
2024-11-27 13:19:29 +0700

    update

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const Route = express.Router()
 Route
     .use("/api/v1/auth", auth)
     .use("/api/v1/admin", admin)
+    .use("/api/v1/administration", administration)
     .use("/api/v1/chat", chat)
     .use("/api/v1/event", event)
     .use("/api/v1/media", media)
