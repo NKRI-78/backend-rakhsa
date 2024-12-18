713192afb7197e2f6b183de4ba9c52dc5afae593
Reihan Agam <reihanagam7@gmail.com>
2024-12-19 01:47:44 +0700

    update

--- index.js
+++ index.js
@@ -14,13 +14,7 @@ const News = require("./src/models/News")
 app.use(fileUpload())
 app.use(logger("dev"))
 app.use(helmet())
-app.use(cors(
-  {
-    origin:'http://localhost:5811', 
-    credentials:true,
-    optionSuccessStatus:200
-  }  
-))
+app.use(cors())
 app.use(compression())
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
