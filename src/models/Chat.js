377e2b69f9205ea584fedf19d1f82d20b2a99fb9
Reihan Agam <reihanagam7@gmail.com>
2024-12-23 18:21:25 +0700

    update

--- src/models/Chat.js
+++ src/models/Chat.js
@@ -160,24 +160,6 @@ module.exports = {
 
     getMessages: (chatId, sender, isAgent) => {
         return new Promise ((resolve, reject) => {
-            // const query = `SELECT 
-            // m.uid,
-            // u.name AS sender_name,
-            // m.sender_id, m.receiver_id,
-            // m.sent_time, m.content, m.image, 
-            // emt.name type, m.is_read
-            // FROM messages m 
-            // LEFT JOIN products p ON m.product_id = p.uid
-            // LEFT JOIN soft_delete_messages sdm ON sdm.message_id = m.uid
-            // INNER JOIN users u ON m.sender_id = u.uid
-            // INNER JOIN chats c ON c.uid = m.chat_id
-            // INNER JOIN enum_message_types emt ON emt.uid = m.type
-            // WHERE c.uid = '${chatId}' 
-            // AND (m.sender_id = '${sender}' 
-            // OR m.receiver_id = '${sender}')
-            // AND (sdm.message_id IS NULL OR sdm.user_id != '${sender}')
-            // ORDER BY m.sent_time DESC`
-
             var query = `SELECT 
             p.fullname AS sender_name,
             p.avatar,
@@ -190,8 +172,7 @@ module.exports = {
             INNER JOIN message_acks ma ON ma.id = m.ack
             INNER JOIN message_types mt ON mt.id = m.type
             WHERE c.uid = ? 
-            AND (m.sender_id = ? 
-            OR m.receiver_id = ?)
+            AND (m.sender_id = ? OR m.receiver_id = ?)
             AND m.is_expired = 0
             ORDER BY m.created_at DESC`
 
@@ -208,8 +189,7 @@ module.exports = {
                 INNER JOIN message_acks ma ON ma.id = m.ack
                 INNER JOIN message_types mt ON mt.id = m.type
                 WHERE c.uid = ? 
-                AND (m.sender_id = ? 
-                OR m.receiver_id = ?)
+                AND (m.sender_id = ? OR m.receiver_id = ?)
                 AND m.is_expired = 0
                 ORDER BY m.created_at DESC`
             }
