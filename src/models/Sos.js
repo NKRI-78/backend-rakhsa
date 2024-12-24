f5b6b8600f7ef140db34cbd110f0f0a00878a637
Reihan Agam <reihanagam7@gmail.com>
2024-12-24 16:42:01 +0700

    update

--- src/models/Sos.js
+++ src/models/Sos.js
@@ -31,7 +31,7 @@ module.exports = {
             FROM 
                 sos
             WHERE 
-                sos_activity_type = 4
+                sos_activity_type IN (4)
             GROUP BY 
                 sos_activity_type`
             conn.query(query, (e, result) => {
@@ -52,7 +52,7 @@ module.exports = {
             FROM 
                 sos
             WHERE 
-                sos_activity_type = 5
+                sos_activity_type IN (5)
             GROUP BY 
                 sos_activity_type`
             conn.query(query, (e, result) => {
